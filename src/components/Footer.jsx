b7fe5f9a41cf9c4f2b0ff1167fefaff8d4a325b6
K.D.S. Akash <akashmusix01@gmail.com>
2024-03-06 14:39:32 +0530

    Initial react app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>&copy; {new Date().getFullYear()} iServeSL</p>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <h1>iServeSL</h1>
+        <nav>
+            <ul>
+                <li><a href='#'>Home</a></li>
+                <li><a href='#'>Customer Support</a></li>
+                <li><a href='#'>Services</a></li>
+                <li><a href='#'>My Account</a></li>
+            </ul>
+        </nav>
+        <hr></hr>
+    </header>
+  );
+}
+
+export default Header;
